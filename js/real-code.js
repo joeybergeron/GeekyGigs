6da8af2e3f952cfb31ba267b389ac1247fc4fc90
graym <yamawarrior@gmail.com>
2014-08-12 12:53:05 -0500

    some progress on Auth Jobs API

--- js/real-code.js
+++ js/real-code.js
@@ -1,21 +1,73 @@
-var JobsCollection = Backbone.Collection.extend({
+var DiceJobsCollection = Backbone.Collection.extend({
 	initialize:function(options){
 		this.location = options.location;
 		this.description = options.description;
 	},
 	url: function(){
-		return "http://service.dice.com/api/rest/jobsearch/v1/simple.json?text="+this.description+"&city="+this.location;
+		return "/jobsearch/"+this.description+"/"+this.location;
+	},
+	parse: function(data){
+		console.log(data);
+		this.count = data.count;
+		this.firstDocument = data.firstDocument;
+		this.lastDocument = data.lastDocument;
+		this.nextUrl = data.nextUrl;
+		return data.resultItemList;
 	}
 })
 
+var AuthenticJobsCollection = Backbone.Collection.extend({
+	initialize:function(options){
+		this.location = options.location;
+		this.keywords = options.keywords;
+		this.perpage = options.perpage;
+		this.page = options.page;
+		this.sort = options.sort;
+	},
+	url: function(){
+		var url = [
+			"/authenticjobs",
+			"?api_key=", 
+			"7770219dad2350459bd66d5c3bf70485",
+			"&method=",
+			"aj.jobs.search",
+			"&format=",
+			"json"
+		]
+		
+		if(this.keywords){
+	        url.push("&keywords=", this.keywords);
+	    }
+	    if(this.location){
+	        url.push("&location=",this.location);
+	    }
+	    if(this.perpage){
+	        url.push("&perpage=",this.perpage);
+	    }
+	    if(this.page){
+	        url.push("&page=",this.page);
+	    }
+	    if(this.sort){
+	        url.push("&sort=",this.sort);
+	    }
 
+		return url.join("")
+	},
+	parse: function(data){
+		console.log(data);
+		return data.listings.listing;
+	}
+})
 
 window.onload = app;
 
 function app(){
-	testCollection = new JobsCollection({
-		location:"77003", 
-		description: "JavaScript" 
+	testCollection = new AuthenticJobsCollection({
+		location:"houston", 
+		keywords: "javascript",
+		perpage: 100,
+		page: 1,
+		// sort: ""
 	});
 	testCollection.fetch();
 }
--- server.js
+++ server.js
@@ -8,7 +8,18 @@ var express = require('express'),
     app = express(),
     myth = require('myth'),
     fs = require('fs'),
-    methodOverride = require('method-override');
+    methodOverride = require('method-override'),
+    httpProxy = require('http-proxy'),
+    request = require('request'),
+    colors = require('colors');
+
+function reportProxy(req, url){
+    console.log('proxying', req.originalUrl.blue ,'to', url.magenta);
+}
+
+function cacheIt(){
+    console.log(arguments)
+}
 
 // all environments
 app.set('port', process.argv[2] || process.env.PORT || 3000);
@@ -31,6 +42,58 @@ app.get("*.css", function(req, res) { //*.css
         }
     });
 });
+
+app.get('/jobsearch/:description/:location', function(req, res){
+    var url = 
+        [
+            "http://service.dice.com/api/rest/jobsearch/v1/simple.json",
+            "?text=",
+            req.params.description,
+            "&city=",
+            req.params.location,
+            "&sort=",
+            1
+        ].join("")
+
+    reportProxy(req, url);
+
+    req.pipe(request(url)).pipe(res);
+})
+
+app.get('/authenticjobs/', function(req, res){
+    var url = 
+        [
+            "http://www.authenticjobs.com/api/",
+            "?api_key=", 
+            req.query.api_key,
+            "&method=",
+            req.query.method,
+            "&format=",
+            req.query.format
+        ];
+
+    if(req.query.keywords){
+        url.push("&keywords=", req.query.keywords);
+    }
+    if(req.query.location){
+        url.push("&location=",req.query.location);
+    }
+    if(req.query.perpage){
+        url.push("&perpage=",req.query.perpage);
+    }
+    if(req.query.page){
+        url.push("&page=",req.query.page);
+    }
+    if(req.query.sort){
+        url.push("&sort=",req.query.sort);
+    }
+
+    url = url.join("");
+
+    reportProxy(req, url);
+
+    req.pipe(request(url)).pipe(res);
+})
 
 app.use(express.static(path.join(__dirname, '')));
 
