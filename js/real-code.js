d309edd0e62b0ef4abff5106da38f0628f99538b
graym <yamawarrior@gmail.com>
2014-08-25 18:40:19 -0500

    stick

--- js/real-code.js
+++ js/real-code.js
@@ -18,6 +18,21 @@
 
 ////////////////////// VIEW ////////////////////////
 
+$(document).ready(function() {
+    var s = $("#searchForm");
+    
+    var pos = s.position();
+    
+    $(window).scroll(function() {
+        var windowpos = $(window).scrollTop();
+
+        if (windowpos >= pos.top) {
+            s.addClass("stick");
+        } else {
+            s.removeClass("stick"); 
+        }
+    });
+});
 
 var JobsView = Backbone.View.extend({
 
